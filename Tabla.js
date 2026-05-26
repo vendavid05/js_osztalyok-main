ce0c898dcb9a9920f7b59ed521e4d49b80f7265b
vendavi <ven.david05@gmail.com>
2026-05-26 12:33:18 +0200

    Enhance Tabla.js with JSDoc documentation
    Added JSDoc comments for class and methods in Tabla.js.

--- Tabla.js
+++ Tabla.js
@@ -1,16 +1,35 @@
 import Elem from "./Elem.js";
 
+/**
+ * @class Tabla
+ * @classdesc A játéktábla megjelenítéséért és a játékelemek (cellák) kezeléséért felelős osztály.
+ */
 export default class Tabla {
+  /** * @private 
+   * @type {Array<string>} A játéktábla aktuális állapotát tároló tömb (pl. ["X", "O", "", ...]).
+   */
   #lista = [];
+
+  /**
+   * Létrehoz egy új Tabla példányt és kirajzolja a cellákat.
+   * @param {Array<string>} lista - A játéktábla elemeit tartalmazó tömb.
+   * @param {HTMLElement} szuloElem - A szülő DOM elem, amelybe a tábla és a cellák kerülnek.
+   */
   constructor(lista, szuloElem) {
     this.#lista = lista;
+    /** @type {HTMLElement} A szülő DOM elem referenciája. */
     this.szuloElem = szuloElem;
     this.megjelenit();
   }
 
+  /**
+   * Kiüríti a szülő elemet, majd végigiterál a listán, 
+   * és minden egyes elemhez létrehoz egy új Elem példányt.
+   */
   megjelenit() {
     this.szuloElem.innerHTML = "";
     this.#lista.forEach((elem, i) => {
+      /** @type {Elem} Egy egyedi cella példány. */
       const k = new Elem(elem, i, this.szuloElem);
     });
   }
